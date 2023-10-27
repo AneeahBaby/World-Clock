a074b4e6ca02c7137a311a4183c8cfa39a452a08
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-10-27 13:42:28 -0500

    switched moment is JS

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,19 @@
-let newyorkElement = document.querySelector("#New-York");
+let newyorkElement = document.querySelector("#new-york");
 let newyorkDateElement = newyorkElement.querySelector(".date");
 let newyorkTimeElement= newyorkElement.querySelector(".time");
 let newyorkTime = moment().tz("America/New_York");
 
 
 newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
-newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]P[</small>]");
+newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]A[</small>]");
+
+let pensacolaElement = document.querySelector("#pensacola");
+let pensacolaDateElement = pensacolaElement.querySelector(".date");
+let pensacolaTimeElement = pensacolaElement.querySelector(".time");
+let pensacolaTime = moment().tz("America/Florida");
+
+pensacolaDateElement.innerHTML = pensacolaTime.format("MMMM Do YYYY");
+pensacolaTimeElement.innerHTML = pensacolaTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+

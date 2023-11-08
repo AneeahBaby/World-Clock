7518070417c07b83e7cfeee2d6562ac2fa2d67ed
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-11-08 14:08:26 -0600

    Edited Html & Java

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,7 @@
+function updateTime() {
+
 let newyorkElement = document.querySelector("#new-york");
+if(newyorkElement) {
 let newyorkDateElement = newyorkElement.querySelector(".date");
 let newyorkTimeElement= newyorkElement.querySelector(".time");
 let newyorkTime = moment().tz("America/New_York");
@@ -6,14 +9,41 @@ let newyorkTime = moment().tz("America/New_York");
 
 newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
 newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
-let pensacolaElement = document.querySelector("#pensacola");
-let pensacolaDateElement = pensacolaElement.querySelector(".date");
-let pensacolaTimeElement = pensacolaElement.querySelector(".time");
-let pensacolaTime = moment().tz("America/Florida");
+let CostaRicaElement = document.querySelector("#CostaRica");
+if(CostaRicaElement) {
+let CostaRicaDateElement = CostaRicaElement.querySelector(".date");
+let CostaRicaTimeElement = CostaRicaElement.querySelector(".time");
+let CostaRicaTime = moment().tz("America/Costa_Rica");
 
-pensacolaDateElement.innerHTML = pensacolaTime.format("MMMM Do YYYY");
-pensacolaTimeElement.innerHTML = pensacolaTime.format(
+CostaRicaDateElement.innerHTML = CostaRicaTime.format("MMMM Do YYYY");
+CostaRicaTimeElement.innerHTML = CostaRicaTime.format(
   "h:mm:ss [<small>]A[</small>]"
-);
+  );
+}
+}
 
+function updateCity(event) {
+let cityTimeZone = event.target.value;
+let cityTime = moment().tz(cityTimeZone);
+let citiesElement = document.querySelector("#cities");
+citiesElement.innerHTML= `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+ }
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addeventlistener("change", updateCity);

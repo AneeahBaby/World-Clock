2e49d7568450a9e417c682fd87977a928bf323d8
AneeahBaby <142260744+AneeahBaby@users.noreply.github.com>
2023-11-11 12:42:09 -0600

    updated Java

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,35 @@
 function updateTime() {
+  let newyorkElement = document.querySelector("#new-york");
+  if (newyorkElement) {
+    let newyorkDateElement = newyorkElement.querySelector(".date");
+    let newyorkTimeElement = newyorkElement.querySelector(".time");
+    let newyorkTime = moment().tz("America/New_York");
 
-let newyorkElement = document.querySelector("#new-york");
-if(newyorkElement) {
-let newyorkDateElement = newyorkElement.querySelector(".date");
-let newyorkTimeElement= newyorkElement.querySelector(".time");
-let newyorkTime = moment().tz("America/New_York");
+    newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
+    newyorkTimeElement.innerHTML = newyorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
+  let CostaRicaElement = document.querySelector("#CostaRica");
+  if (CostaRicaElement) {
+    let CostaRicaDateElement = CostaRicaElement.querySelector(".date");
+    let CostaRicaTimeElement = CostaRicaElement.querySelector(".time");
+    let CostaRicaTime = moment().tz("America/Costa_Rica");
 
-newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
-newyorkTimeElement.innerHTML = newyorkTime.format("h:mm:ss [<small>]A[</small>]");
-}
-
-let CostaRicaElement = document.querySelector("#CostaRica");
-if(CostaRicaElement) {
-let CostaRicaDateElement = CostaRicaElement.querySelector(".date");
-let CostaRicaTimeElement = CostaRicaElement.querySelector(".time");
-let CostaRicaTime = moment().tz("America/Costa_Rica");
-
-CostaRicaDateElement.innerHTML = CostaRicaTime.format("MMMM Do YYYY");
-CostaRicaTimeElement.innerHTML = CostaRicaTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-  );
-}
+    CostaRicaDateElement.innerHTML = CostaRicaTime.format("MMMM Do YYYY");
+    CostaRicaTimeElement.innerHTML = CostaRicaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
-let cityTimeZone = event.target.value;
-let cityTime = moment().tz(cityTimeZone);
-let citiesElement = document.querySelector("#cities");
-citiesElement.innerHTML= `
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
@@ -39,11 +40,11 @@ citiesElement.innerHTML= `
   )}</small></div>
   </div>
   `;
- }
+}
 
 updateTime();
 setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 
-citiesSelectElement.addeventlistener("change", updateCity);
+citiesSelectElement.addEventListener("change", updateCity);
